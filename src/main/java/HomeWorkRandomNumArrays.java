fbdec87591ae4818ee7cea556104c4dd00b4660c
Elnur <eeliyev@yahoo.com>
2019-10-17 23:17:02 +0400

    -- initialRandomNumberAssignment

---
+++ src/main/java/HomeWorkRandomNumArrays.java
@@ -0,0 +1,11 @@
+import java.util.Random;
+
+public class HomeWorkRandomNumArrays
+{
+    public static void main(String[] args)
+    {
+    Random number= new Random();
+
+        System.out.println(number.nextInt(15));
+    }
+}
