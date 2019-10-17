e20adeab3d27e427b6ee204b105f1f2b37865de2
Elnur <eeliyev@yahoo.com>
2019-10-17 23:28:34 +0400

    -- SecondInputScanner

--- src/main/java/HomeWorkRandomNumArrays.java
+++ src/main/java/HomeWorkRandomNumArrays.java
@@ -1,11 +1,19 @@
 import java.util.Random;
+import java.util.Scanner;
+
 
 public class HomeWorkRandomNumArrays
 {
     public static void main(String[] args)
     {
     Random number= new Random();
+    System.out.println(number.nextInt(15));
 
-        System.out.println(number.nextInt(15));
+    Scanner PlayerName= new Scanner(System.in);
+    System.out.print("Please enter your name: ");
+    String Name = PlayerName.nextLine();
+    System.out.println("Player 1 is: " + Name);
+
+
     }
 }
